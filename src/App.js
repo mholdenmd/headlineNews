ff0c92a9d5b9ab462c62c96ae2496481375eba11
mholdenmd <77797864+mholdenmd@users.noreply.github.com>
2021-08-16 17:32:01 -0400

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState}  from 'react';
+
 
 function App() {
+
+  const [topHeadlines, setTopHeadlines] = useState([])
+
+  
+  const getNews = () => {
+    console.log("clicked!")
+
+    fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=bf9482c6b383478781980d699f27e9de")
+    .then(response => {
+      console.log("*******")
+      console.log(response)
+      console.log("*******")
+      return response.json()
+    })
+    .then(response => {
+      console.log("!!!!!!!!")
+      console.log(response)
+      console.log("!!!!!!!!")
+      setTopHeadlines(response.articles)
+
+
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {getNews} >Get some news </button>
+      {topHeadlines.map((newsArticle,idx) =>{
+        return <h1>{newsArticle.title}</h1>
+      })}
+      
     </div>
   );
 }
-
+}
 export default App;
+
+
